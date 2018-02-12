bad73ebc9d00a21129ba12b49a6d662ab362ce88
Andrew Toh <andrewtoh@HOPE-L-041>
2018-02-12 17:03:12 +0800

    teset build

--- LegacyWindowsFormsApp/LegacyWindowsFormsApp/Program.cs
+++ LegacyWindowsFormsApp/LegacyWindowsFormsApp/Program.cs
@@ -14,6 +14,8 @@ namespace LegacyWindowsFormsApp
         [STAThread]
         static void Main()
         {
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
